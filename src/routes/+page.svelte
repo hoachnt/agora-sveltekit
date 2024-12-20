9d18b61ec5ed7be7d3c0fd91310d5a018b943ac9
hoachnt <hoachprogrammer@gmail.com>
2024-12-20 14:24:00 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import './styles.css'
+	let channel = 'test'
+	let uid = 0
+	const handleClick = () => {
+		window.location.href = `/call/${channel}?uid=${uid}`
+	}
+</script>
+
+<h1>SvelteKit x Agora</h1>
+<div class="text-column">
+	<label>
+		Channel Name:
+		<input bind:value={channel} name="channel" type="text" />
+	</label>
+	<label>
+		UID:
+		<input bind:value={uid} name="uid" type="number" />
+	</label>
+	<button on:click={handleClick}>Join Channel</button>
+</div>
